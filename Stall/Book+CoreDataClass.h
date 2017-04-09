9b5dfc00b0a3afbe2268ff48c4e890e7881f2935
Shangxin Guo <int123c@gmail.com>
2017-04-09 16:20:55 +0800

    Update Model

---
+++ Stall/Book+CoreDataClass.h
@@ -0,0 +1,20 @@
+//
+//  Book+CoreDataClass.h
+//  Stall
+//
+//  Created by Inti Guo on 09/04/2017.
+//  Copyright © 2017 Inti Guo. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <CoreData/CoreData.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface Book : NSManagedObject
+
+@end
+
+NS_ASSUME_NONNULL_END
+
+#import "Book+CoreDataProperties.h"
---
+++ Stall/Book+CoreDataClass.m
@@ -0,0 +1,13 @@
+//
+//  Book+CoreDataClass.m
+//  Stall
+//
+//  Created by Inti Guo on 09/04/2017.
+//  Copyright © 2017 Inti Guo. All rights reserved.
+//
+
+#import "Book+CoreDataClass.h"
+
+@implementation Book
+
+@end
