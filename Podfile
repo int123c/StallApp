c75df3862497d916650525f893c9e4ee1960269e
Shangxin Guo <int123c@gmail.com>
2017-04-09 15:46:36 +0800

    Add CocoaPods MagicalRecord and AFNetworking

---
+++ Podfile
@@ -0,0 +1,4 @@
+target 'Stall' do 
+	pod 'MagicalRecord'
+	pod 'AFNetworking', '~> 3.0'
+end
---
+++ Pods/Target Support Files/Pods-Stall/Pods-Stall-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Stall : NSObject
+@end
+@implementation PodsDummy_Pods_Stall
+@end
