17f664e476d8edc52c50068d798acacaa95247af
Shangxin Guo <int123c@gmail.com>
2017-04-12 01:22:12 +0800

    Update

--- Stall/AppDelegate.m
+++ Stall/AppDelegate.m
@@ -38,6 +38,7 @@
 - (void)applicationWillResignActive:(UIApplication *)application {
     // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
     // Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should use this method to pause the game.
+    [MagicalRecord cleanUp];
 }
 
 
@@ -61,6 +62,7 @@
     // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
     // Saves changes in the application's managed object context before the application terminates.
     [self saveContext];
+    [MagicalRecord cleanUp];
 }
 
 
