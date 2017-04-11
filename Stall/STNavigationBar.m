ee297100b41bb2ca863e2a827cf0369803c668ee
Shangxin Guo <int123c@gmail.com>
2017-04-11 16:33:01 +0800

    Update

---
+++ Stall/STNavigationBar.m
@@ -0,0 +1,21 @@
+//
+//  STNavigationBar.m
+//  Stall
+//
+//  Created by Inti Guo on 11/04/2017.
+//  Copyright © 2017 Inti Guo. All rights reserved.
+//
+
+#import "STNavigationBar.h"
+
+@implementation STNavigationBar
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
---
+++ Stall/ScanNavigationController.h
@@ -0,0 +1,13 @@
+//
+//  ScanNavigationController.h
+//  Stall
+//
+//  Created by Inti Guo on 11/04/2017.
+//  Copyright © 2017 Inti Guo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ScanNavigationController : UINavigationController
+
+@end
